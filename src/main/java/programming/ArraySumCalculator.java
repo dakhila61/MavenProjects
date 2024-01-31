99ff1a10d9f38dda73dec8fd1a5ccf788f62bc63
adharavath61 <adharavath@my.okcu.edu>
2024-01-31 16:06:21 -0600

    Basic programs

---
+++ src/main/java/programming/ArraySumCalculator.java
@@ -0,0 +1,13 @@
+package programming;
+
+public class ArraySumCalculator {
+
+    public int calculateArraySum(int[] arr) {
+        int sum = 0;
+        // Iterate through the array and add each element to the sum
+        for (int num : arr) {
+            sum += num;
+        }
+        return sum;
+    }
+}
